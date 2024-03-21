fe2606de4f4bfbf189612776fe194d264e26ab0a
medazza <midazza27@gmail.com>
2024-03-21 23:04:50 +0000

    configur docker

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+ENV = os.environ.get("ENV")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +27,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^i!nlxcomdanxm$%#dwn!bjg)rmtwpw&@!_9#&+oepni!%-37y'
+# SECRET_KEY = 'django-insecure-^i!nlxcomdanxm$%#dwn!bjg)rmtwpw&@!_9#&+oepni!%-37y'
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", default=
+    'django-insecure-^i!nlxcomdanxm$%#dwn!bjg)rmtwpw&@!_9#&+oepni!%-37y'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False if ENV == "PROD" else True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS",
+                    default="*").split(",")
 
 # Application definition
 
@@ -95,15 +109,26 @@ WSGI_APPLICATION = 'CoreRoot.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#     'ENGINE': "django.db.backends.postgresql_psycopg2",
+#         'NAME': "coredb",
+#         'USER': "core",
+#         'PASSWORD': "wCh29&HE&T83",
+#         'HOST': "localhost",
+#         'PORT': "5432",
+#     }
+# }
+
 DATABASES = {
-    'default': {
-    'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'NAME': "coredb",
-        'USER': "core",
-        'PASSWORD': "wCh29&HE&T83",
-        'HOST': "localhost",
-        'PORT': "5432",
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME", "coredb"),
+        "USER": os.getenv("DATABASE_USER", "core"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "wCh29&HE&T83"),
+        "HOST": os.environ.get("DATABASE_HOST", "localhost"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
+        }
 }
 
 
